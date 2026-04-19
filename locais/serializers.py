b3099f6f4230d9d317e01f2676b316d031a4da67
acessoja <acessoaja@gmail.com>
2026-04-19 12:03:38 -0300

    Revert "commit new changes due to ciclo 2"
    This reverts commit e76043f272c47100a2c0523d62a30133b1eaab51.

--- locais/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Local
-
-class LocalSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Local
-        fields = ['id_local', 'nome', 'endereco', 'distancia', 'latitude', 'longitude', 'aberto', 'data_criacao']
--- main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Página inicial (login)
-    path('home/', views.home, name='home'),  # Página inicial pós-login
-    path('login.html', views.index, name='login'),  # Rota para acessar login.html diretamente
-]
